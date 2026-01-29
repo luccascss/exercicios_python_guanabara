3c23653fc6948dd4cf082e5dc7524e0c840f7498
Lucas Silva <lucascss11@gmail.com>
2026-01-29 19:03:06 -0300

    refactor: improve conditional logic
    - improve validation for NUM variable
    - refactor code structure without change logic

--- ex060.py
+++ ex060.py
@@ -1,11 +1,16 @@
 #fatorial usando while
-num = int(input('Digite um número inteiro: '))
+num = int(input('Digite um número inteiro para calcular seu Fatorial: '))
 resultado = 1
 fatorial = num
-while fatorial != 1:
-    if num <= 0:
-        num = int(input('Digite um número válido: '))
-    else:
-        resultado = resultado * fatorial
-        fatorial = fatorial - 1
-print(f'O resultado da expressão {num}! é {resultado}')
+if num < 0:
+    while num < 0:
+        num = int(input('Tente novamente com um número válido: '))
+else:
+    print(f'Calculando...')
+    print(f'{num}! = ', end='')
+    while fatorial > 0:
+            print(f'{fatorial}', end='')
+            print(' x ' if fatorial > 1 else ' = ', end= '')
+            resultado = resultado * fatorial
+            fatorial = fatorial - 1
+print(f'{resultado}')
