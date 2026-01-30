db6dc489824368f92ada057879da7fbc2a2d64d2
Lucas Silva <lucascss11@gmail.com>
2026-01-30 16:14:45 -0300

    refactor: improve variable names and readability
    - change variable names
    - adjust values without changing logic
    - improve code readability

--- ex061.py
+++ ex061.py
@@ -1,11 +1,11 @@
 #dez primeiros termos de uma PA
 primeiro_termo = float(input('Digite o primeiro termo: '))
 razao = float(input('Digite a razão: '))
-termo = 10
-enesimo_termo = primeiro_termo - razao
+contador = 0
+termo = primeiro_termo - razao
 print(f'A PA escolhida é: (',end='')
-while termo > 0:
-    termo = termo - 1
-    enesimo_termo = enesimo_termo + razao
-    print(f'{enesimo_termo}', end= '')
-    print(')' if termo < 1 else ', ', end= '')
+while contador < 10:
+    contador = contador + 1
+    termo = termo + razao
+    print(f'{termo}', end= '')
+    print(')' if contador > 9 else ', ', end= '')
