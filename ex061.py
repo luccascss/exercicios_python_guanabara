e049b9dff0710f17c27460e916efcf836b9bc1b0
Lucas Silva <lucascss11@gmail.com>
2026-01-30 16:24:57 -0300

    style: improve print layout

--- ex061.py
+++ ex061.py
@@ -1,4 +1,5 @@
 #dez primeiros termos de uma PA
+print('O programa a seguir analisará o primeiro termo e a razão e mostrará sua PA em 10 termos:')
 primeiro_termo = float(input('Digite o primeiro termo: '))
 razao = float(input('Digite a razão: '))
 contador = 0
@@ -8,4 +9,4 @@ while contador < 10:
     contador = contador + 1
     termo = termo + razao
     print(f'{termo}', end= '')
-    print(')' if contador > 9 else ', ', end= '')
+    print(')' if contador > 9 else ' ➡ ', end= '')
