9ba87e529f16679dac74a4c1901696c7c123aef8
Lucas Silva <lucascss11@gmail.com>
2026-01-31 21:43:50 -0300

    fix: correct fibonacci sequence output and simplify logic
    - first two terms of sequence (0 and 1) are now included
    - simplified logic by removing unnecessary input loop

--- ex063.py
+++ ex063.py
@@ -1,28 +1,31 @@
 #sequência de Fibonacci
 print('Descubra a sequência de Fibonacci: ')
 num = int(input('Quantos elementos da sequência deseja ver? '))
-f_zero = 0
-f_um = 1
-f = 0
-contador = 1
-total = 0
-while num != 0:
-    if num > 0:
-        total = total + num
-        while contador <= total:
+t_pu = 0 #penúltimo termo
+t_u = 1 #último termo
+t_n = 0 #n-ésimo termo (termo definido pelo input)
+contador = 3 #começa no 3 pois os 2 primeiros termos da sequência são fixos (0 e 1)
+if num > 0:
+    if num == 1:
+        print('0 ➡ FIM')
+    elif num == 2:
+        print('0 ➡ 1 ➡ FIM')
+    elif num >= 3:
+        print(f'0 ➡ 1 ➡ ', end='') #primeiros termos da sequência
+        while contador <= num:
             contador = contador + 1
-            f = f_zero + f_um
-            if contador <= total:
-                print(f'{f} > ', end='')
+            t_n = t_pu + t_u
+            if contador <= num:
+                print(f'{t_n} ➡ ', end='')
             else:
-                print(f'{f} > ', end='')
-                print('PAUSA')
-            f_zero = f_um
-            f_um = f
-        num = int(input('Quantos elementos da sequência deseja ver? '))
-    if num == 0:
-        print('Finalizando...')
-    elif num < 0:
-        num = int(input('Digite um valor válido: '))
-print(f'O programa foi finalizado com {total} números da Sequência de Fibonacci demostrados!')
+                print(f'{t_n} ➡ ', end='')
+                print('FIM')
+            t_pu = t_u
+            t_u = t_n
+elif num == 0:
+    print('Finalizando sem nenhum termo...')
+elif num < 0:
+    print('Tente novamente com um valor válido.')
+print('-' * 50)
+print('O programa foi finalizado!')
 
