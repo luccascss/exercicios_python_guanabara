13c0428509f7e8d85a3e7a684b4606306fc5e090
Lucas Silva <lucascss11@gmail.com>
2026-02-05 13:25:51 -0300

    feat: add exercise 073 with tuples

---
+++ ex073.py
@@ -0,0 +1,12 @@
+#tabela do campeonato brasileiro com tupla
+lista_times = ('bragantino', 'palmeiras', 'mirassol', 'são paulo', 'botafogo',
+               'chapecoense', 'grêmio', 'fluminense', 'bahia', 'athletico-pr',
+               'vitória','flamengo', 'atlético-mg', 'internacional', 'santos',
+               'remo', 'vasco', 'corinthians', 'coritiba', 'cruzeiro')
+print(f'Os 5 primeiros colocados do BR-2026 são: {lista_times[0:5]}')
+print('=' * 30)
+print(f'Os 4 últimos colocados são: {lista_times[16:20]}')
+print('=' * 30)
+print(f'Os times em ordem alfabética: {sorted(lista_times)}')
+print('=' * 30)
+print(f'A Chapecoense está na {lista_times.index('chapecoense')}ª colocação.')
