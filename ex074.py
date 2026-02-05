bd3ce99d0a9b082acb27f7a7112fb601c9f0d7e1
Lucas Silva <lucascss11@gmail.com>
2026-02-05 14:27:41 -0300

    refactor: remove multiples randint calls
    - use "for _ in" instead repeat randint

--- ex074.py
+++ ex074.py
@@ -1,6 +1,7 @@
 #gerar 5 números aleatórios e armazenar em tupla, informando o maior e menor valor
 from random import randint
-numeros = (randint(0, 10), randint(0, 10), randint(0, 10), randint(0, 10), randint(0, 10))
+numeros = tuple(randint(1,10) for _ in range(5))
+#usar " _ " quando o valor de for não for relevante para o código
 print(f'Os valores sorteados foram: {numeros}')
 print(f'O maior valor foi: {max(numeros)}')
 print(f'O menor valor foi: {min(numeros)}')
