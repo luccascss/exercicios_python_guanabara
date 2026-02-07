fc181e4c6cefbfd21f27bb8d430612d3076bda3f
Lucas Silva <lucascss11@gmail.com>
2026-02-07 17:30:32 -0300

    refactor: improve variable names and readability
    - add comments before each for loop and conditional
    - rename variables

--- ex077.py
+++ ex077.py
@@ -1,11 +1,13 @@
 # leia palavavras dentro de uma tupla e mostre apenas as suas vogais
-tupla = ('aprender', 'programar', 'linguaguem', 'python', 'curso',
+tupla_palavras = ('aprender', 'programar', 'linguaguem', 'python', 'curso',
          'gratis', 'estudar', 'praticar', 'trabalhar', 'mercado',
          'programador', 'futuro')
 vogais = 'aeiou'
-for c in range(0, len(tupla)):
-    print(f'Na palavra {tupla[c].upper()} temos: ',end='')
-    for letra in tupla[c]:
+# separa a tupla em palavras
+for palavra in tupla_palavras:
+    print(f'\nNa palavra {palavra.upper()} temos: ',end='')
+    # separa cada palavra em letras
+    for letra in palavra:
+        # verifica se letra é uma vogal
         if letra in vogais:
             print(f'{letra} ', end='')
-    print()
