fcc62ff597c27de90f9df81107d240b133fba639
Lucas Silva <lucascss11@gmail.com>
2026-02-06 21:01:22 -0300

    feat: add exercise 076 with tuples

---
+++ ex076.py
@@ -0,0 +1,9 @@
+#tupla ÚNICA com nome e preço de produtos. No final, mostre uma tabela organizando seus valores
+produtos = ('Caneta', 2.50, 'Borracha', 1.00, 'Estojo', 10.50, 'Compasso', 9.99, 'Livro', 34.90)
+print('Listagem de preços:')
+print(f'{produtos[0]}----------------R${produtos[1]:.2f}')
+print(f'{produtos[2]}--------------R${produtos[3]:.2f}')
+print(f'{produtos[4]}----------------R${produtos[5]:.2f}')
+print(f'{produtos[6]}--------------R${produtos[7]:.2f}')
+print(f'{produtos[8]}-----------------R${produtos[9]:.2f}')
+
