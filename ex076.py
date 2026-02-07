86468ac24ce22caeef2765a2bbcea877e2e5d55e
Lucas Silva <lucascss11@gmail.com>
2026-02-06 21:15:26 -0300

    feat: define produts and prices using for loop
    - remove manual output
    - add dynamic output using for loop

--- ex076.py
+++ ex076.py
@@ -1,9 +1,12 @@
 #tupla ÚNICA com nome e preço de produtos. No final, mostre uma tabela organizando seus valores
 produtos = ('Caneta', 2.50, 'Borracha', 1.00, 'Estojo', 10.50, 'Compasso', 9.99, 'Livro', 34.90)
-print('Listagem de preços:')
-print(f'{produtos[0]}----------------R${produtos[1]:.2f}')
-print(f'{produtos[2]}--------------R${produtos[3]:.2f}')
-print(f'{produtos[4]}----------------R${produtos[5]:.2f}')
-print(f'{produtos[6]}--------------R${produtos[7]:.2f}')
-print(f'{produtos[8]}-----------------R${produtos[9]:.2f}')
+print('=' * 40)
+print(f'{"Listagem de preços":^40}')
+print('=' * 40)
+for posicao in range(0, len(produtos)):
+    if posicao % 2 == 0:
+        print(f'{produtos[posicao]:.<30}', end='')
+    else:
+        print(f'R${produtos[posicao]:.2f}')
+print('-' * 40)
 
