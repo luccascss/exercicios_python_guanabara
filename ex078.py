4fe8df80a7e68284a46bc33d1451acfb60055648
Lucas Silva <lucascss11@gmail.com>
2026-02-10 17:34:30 -0300

    fix: correct max and min index handling

--- ex078.py
+++ ex078.py
@@ -2,6 +2,15 @@
 valores = list()
 for p in range(5):
     valores.append(int(input(f'Digite um número para a posição {p}: ')))
+valor_max = max(valores)
+valor_min = min(valores)
 print(f'Lista com os valores: {valores}')
-print(f'O maior valor digitado foi {max(valores)} e sua posição na lista é {valores.index(max(valores))}')
-print(f'O menor valor digitado foi {min(valores)} e sua posição na lista é {valores.index(min(valores))}')
+print(f'O maior valor digitado foi {max(valores)} nas posições ',end='')
+for e, v in enumerate(valores):
+    if valor_max == v:
+        print(f'{e}... ', end='')
+print(f'\nO menor valor digitado foi {min(valores)} nas posições ',end='')
+for e, v in enumerate(valores):
+    if valor_min == v:
+        print(f'{e}... ', end='')
+
