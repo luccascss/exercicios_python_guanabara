47d80564e84c04f00359529a13489f2090d95402
Lucas Silva <lucascss11@gmail.com>
2026-02-12 18:16:56 -0300

    fix: logic problem resolved

--- ex080.py
+++ ex080.py
@@ -1,12 +1,16 @@
 #coloque 5 valores em uma lista em ordem crescente sem utilizar o método sort
 lista_num = list()
-for _ in range (5):
-    num = (int(input('Digite um valor: ')))
-    for n in lista_num:
-        if num >= n:
-            lista_num.append(num)
-            print('Valor adicionado ao final da lista...')
-        elif num < n:
-            lista_num.insert(0, num)
-            print('Valor adicionado na posição 0 da lista...')
+for c in range(5):
+    num = (int(input(f'Digite o {c + 1}° valor: ')))
+    if c == 0 or num > lista_num[-1]:
+        lista_num.append(num)
+        print('Adicionado ao final da lista...')
+    else:
+        pos = 0
+        while pos < len(lista_num):
+            if num <= lista_num[pos]:
+                lista_num.insert(pos, num)
+                print(f'Adicionado na posição {pos} da lista...')
+                break
+            pos = pos + 1
 print(lista_num)
