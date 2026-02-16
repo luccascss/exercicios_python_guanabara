9e7bdf9f15c538a910d2683aa10b588bcb0e2015
Lucas Silva <lucascss11@gmail.com>
2026-02-16 18:54:36 -0300

    refactor: simplify even and odd number grouping logic

--- ex085.py
+++ ex085.py
@@ -1,18 +1,12 @@
 # Leia 7 valores numéricos e os coloque numa única lista que mantenha os valores pares e ímpares separadas. No fim, mostre os valores em ordem crescente.
-num_lista = list() # par / ímpar respectivamente
-par =  list()
-impar = list()
-cont = 0
-for _ in range (7):
-    cont = cont + 1
-    num = int(input(f'Digite o {cont}° valor: '))
+num_lista = [[], []] # par / ímpar respectivamente
+for c in range (1, 8):
+    num = int(input(f'Digite o {c}° valor: '))
     if num % 2 == 0: #par
-        par.append(num)
+        num_lista[0].append(num)
     if num % 2 != 0: #ímpar
-        impar.append(num)
-par.sort()
-impar.sort()
-num_lista.insert(0, par[:])
-num_lista.insert(1, impar[:])
+        num_lista[1].append(num)
+num_lista[0].sort()
+num_lista[1].sort()
 print(f'Valores pares: {num_lista[0]}')
 print(f'Valores ímpares: {num_lista[1]}')
