a4ebd255fb4bfeac694a0ede80445c3390fab47e
Lucas Silva <lucascss11@gmail.com>
2026-02-16 16:28:24 -0300

    refactor: improve logic for tracking highest and lowest weighits
    - improve variable names
    - reduce loop numbers

--- ex084.py
+++ ex084.py
@@ -1,10 +1,10 @@
 # Leia vários inputs com nome e peso guardando numa lista. Depois, diga quantas pessoas foram cadastradas, quais as mais pesadas e quais a mais leves
+dados_temp = list()
 dados = list()
-lista_dados = list()
 resposta = ''
-cont_pessoas = 0
-lista_pessoas = list()
-lista_peso = list()
+maior_peso = 0
+menor_peso = 0
+pessoas = list()
 nome_peso_maior = list()
 nome_peso_menor = list()
 while True:
@@ -13,10 +13,18 @@ while True:
     if peso < 0:
         print('Idade digitada inválida.')
     else:
-        dados.append(nome)
-        dados.append(peso)
-        lista_dados.append(dados[:])
-        dados.clear()
+        dados_temp.append(nome)
+        dados_temp.append(peso)
+        if len(dados) == 0:
+            maior_peso = peso
+            menor_peso = peso
+        else:
+            if peso > maior_peso:
+                maior_peso = peso
+            elif peso < menor_peso:
+                menor_peso = peso
+        dados.append(dados_temp[:])
+        dados_temp.clear()
     while resposta != 'S' and resposta != 'N':
         resposta = str(input('Deseja continuar? [S/N] ')).strip().upper()
         if resposta != 'S' and resposta != 'N':
@@ -26,15 +34,12 @@ while True:
         resposta = ''
     elif resposta == 'N':
         break
-for e, p in enumerate(lista_dados):
-    cont_pessoas += 1
-    lista_pessoas.append(lista_dados[e][0])
-    lista_peso.append(lista_dados[e][1])
-for e, p in enumerate(lista_dados):
-    if max(lista_peso) == lista_dados[e][1]:
-        nome_peso_maior.append(lista_dados[e][0])
-    elif min(lista_peso) == lista_dados[e][1]:
-        nome_peso_menor.append(lista_dados[e][0])
-print(f'Foram cadastradas {cont_pessoas} pessoas: {lista_pessoas}')
-print(f'Maior peso foi de {max(lista_peso)}: {nome_peso_maior}')
-print(f'Menor peso foi de {min(lista_peso)}: {nome_peso_menor}')
+for p in dados:
+    pessoas.append(p[0])
+    if maior_peso == p[1]:
+        nome_peso_maior.append(p[0])
+    elif menor_peso == p[1]:
+        nome_peso_menor.append(p[0])
+print(f'Foram cadastradas {len(pessoas)} pessoas: {pessoas}')
+print(f'Maior peso foi de {maior_peso}Kg: {nome_peso_maior}')
+print(f'Menor peso foi de {menor_peso}Kg: {nome_peso_menor}')
