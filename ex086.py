10d408efc462a560af20d4cb32096f3fd89c724f
Lucas Silva <lucascss11@gmail.com>
2026-02-16 19:32:53 -0300

    refactor: improved loop strucuture

--- ex086.py
+++ ex086.py
@@ -1,8 +1,9 @@
 # crie uma matriz 3x3 e depois mostre-a na tela com a formatação correta
-matriz = [[], [], [], [], [], [], [], [], []]
-for c in range(0, 9):
-    valor = int(input(f'Digite o {c + 1}° valor: '))
-    matriz[c].append(valor)
-print(f'{matriz[0]}{matriz[1]}{matriz[2]}')
-print(f'{matriz[3]}{matriz[4]}{matriz[5]}')
-print(f'{matriz[6]}{matriz[7]}{matriz[8]}')
+matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+for l in range(0, 3):
+    for p in range(0, 3):
+        matriz[l][p] = int(input(f'Digite o valor para [{l}, {p}]: '))
+for l in range(0, 3):
+    for p in range(0, 3):
+        print(f'[{matriz[l][p]:^5}]', end='')
+    print()
