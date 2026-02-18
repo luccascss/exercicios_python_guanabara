1576ff3e0622f6d2257b37f0a797391cbcfa31bf
Lucas Silva <lucascss11@gmail.com>
2026-02-18 17:12:46 -0300

    refactor: simplify append for list
    - add discription to variable
    - simplify append for list

--- ex089.py
+++ ex089.py
@@ -1,20 +1,13 @@
 # Guarde lista dentro de listas dentro de lista
-dados = list()
-nome_temp = list()
-nota_temp = list()
+dados = list() # [nome[0], notas[1], média[2]], [nome[0], notas[1], média[2]], ...
 resp = ''
 print('========== COLÉGIO GALOTITO ===========')
 while True:
     nome = str(input('Nome: ')).strip().upper()
     nota_01 = float(input('1ª nota: '))
     nota_02 = float(input('2ª nota: '))
-    nome_temp.append(nome) # add nome na lista temporária
-    nota_temp.append(nota_01) # add nota na lista temporária
-    nota_temp.append(nota_02) # add nota na lista temporária
-    nome_temp.append(nota_temp[:]) # add lista de notas na lista de nome
-    dados.append(nome_temp[:]) # add todos os dados na lista principal
-    nome_temp.clear() # limpar a lista de nome temporária
-    nota_temp.clear() #limpar a lista de notas temporária
+    media = ((nota_01 + nota_02) / 2)
+    dados.append([nome, [nota_01, nota_02], media])
     while resp != 'S' and resp != 'N':
         resp = str(input('Deseja continuar? [S/N] ')).strip().upper()
         if resp != 'S' and resp != 'N':
@@ -28,7 +21,7 @@ print('=' * 30)
 print('REFERÊNCIA (N°: NOME -> MÉDIA)')
 print('-' * 30)
 for i, d in enumerate(dados):
-    print(f'{i + 1}: {d[0]} -> {((d[1][0] + d[1][1]) / 2)}')
+    print(f'{i + 1}: {d[0]} -> {d[2]}')
 print('-' * 80)
 print('Escolha um número referente a um aluno para ver suas notas separadamente:')
 num = -1 # variável de controle do loop
