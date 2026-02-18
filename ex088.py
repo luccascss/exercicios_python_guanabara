d765313b9d1f0114b7131c46e895ec33455fdf06
Lucas Silva <lucascss11@gmail.com>
2026-02-18 16:54:16 -0300

    refactor: chance randint for sample to generate numbers

--- ex088.py
+++ ex088.py
@@ -1,21 +1,16 @@
 # Sorteie números da mega sena
-from random import randint
+from random import sample
 from time import sleep
 jogos = list()
-jogo_temp = list()
 print('-' * 40)
 print(f'{'GERADOR DE NÚMEROS - MEGA SENA':^40}')
 print('-' * 40)
 num = int(input('Quantos jogos você deseja sortear? '))
 print(f'{'=-' * 5} SORTEANDO {num} JOGOS {'-=' * 5}')
 for j in range (num):
-    for n in range (6):
-        sorteio = randint(1, 60)
-        jogo_temp.append(sorteio)
-    jogo_temp.sort()
-    jogos.append(jogo_temp[:])
-    jogo_temp.clear()
+    sorteio = sample(range(1,61), 6)
+    sorteio.sort()
+    jogos.append(sorteio[:])
     print(f'Jogo {j + 1}: {jogos[j]}')
     sleep(1)
 print('=' * 40)
-
