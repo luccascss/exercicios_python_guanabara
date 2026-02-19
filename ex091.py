ff9b18533000df9ba75a207f322189ae4faee5e8
Lucas Silva <lucascss11@gmail.com>
2026-02-19 18:03:09 -0300

    refactor: change lambda for operator method
    - rename variables
    - create new dictionary "ranking"

--- ex091.py
+++ ex091.py
@@ -1,7 +1,9 @@
 # Adicionando valores de um dado num dicionário e os colocando em ordem crescente
 from random import randint
 from time import sleep
+from operator import itemgetter
 jogo = dict()
+ranking = dict()
 for j in range (1, 5):
     jogo[f'jogador_{j}'] = randint(1,6)
 print('Valores sorteados: ')
@@ -9,6 +11,6 @@ for k, v in jogo.items():
     print(f'   O {k} tirou {v}')
     sleep(1)
 print('Ranking dos Jogadores: ')
-jogo_resultado = sorted(jogo.items(), key=lambda item: item[1], reverse=True)
-for i, (k, v) in enumerate(jogo_resultado):
+ranking = sorted(jogo.items(), key = itemgetter(1), reverse=True)
+for i, (k, v) in enumerate(ranking):
     print(f'   {i + 1}° lugar: {k} com {v}')
