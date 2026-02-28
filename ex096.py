432e6304d6d72658844bcf4410a06e0f60ec5d76
Lucas Silva <lucascss11@gmail.com>
2026-02-28 14:26:32 -0300

    feat: add exercise 096 with function

---
+++ ex096.py
@@ -0,0 +1,11 @@
+ # calcule a área utilizando função
+def area():
+    a = c * l
+    print(f'A área do terreno {c}x{l} é de {a:.1f}m².')
+
+
+#programa principal
+c = float(input('Comprimento (m): '))
+l = float(input('Largura (m): '))
+area()
+
