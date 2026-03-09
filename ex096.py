88e584cafd93793b89788c913a75eb787fe1a175
Lucas Silva <lucascss11@gmail.com>
2026-03-09 14:09:48 -0300

    fix: variable name problem
    - chance variable name in def area()

--- ex096.py
+++ ex096.py
@@ -1,11 +1,11 @@
  # calcule a área utilizando função
-def area():
-    a = c * l
-    print(f'A área do terreno {c}x{l} é de {a:.1f}m².')
+def area(comprimento, largura):
+    a = comprimento * largura
+    print(f'A área do terreno {comprimento}x{largura} é de {a:.1f}m².')
 
 
 #programa principal
 c = float(input('Comprimento (m): '))
 l = float(input('Largura (m): '))
-area()
+area(c, l)
 
