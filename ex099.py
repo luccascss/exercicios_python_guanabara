0be547a44488312e77cb7edd6d8c4eee37486b9e
Lucas Silva <lucascss11@gmail.com>
2026-03-09 14:38:23 -0300

    docs: add comments to explain variable and conditional
    - variable "m" in def maior(lst)
    - conditional to empty list

--- ex099.py
+++ ex099.py
@@ -1,6 +1,6 @@
 # Crie uma função maior() que receba vários parâmetros que possa dizer qual dos valores é o maior
 def maior(lst):
-    m = lst[0]
+    m = lst[0] #maior valor; começa com o primeiro valor da lista como maior valor
     for n in lst:
         if n > m:
             m = n
@@ -13,6 +13,6 @@ while True:
     if num == 999:
         break
     lista_num.append(num)
-if len(lista_num) == 0:
+if len(lista_num) == 0: #regra caso não adicione nenhum valor, adicionando "0" na lista vazia
     lista_num.append(0)
 maior(lista_num)
