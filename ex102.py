3c5cd12b2728a310387ba0695a8b823a223dd1b4
Lucas Silva <lucascss11@gmail.com>
2026-03-20 18:11:11 -0300

    feat: add optional display of factorial calculation steps

--- ex102.py
+++ ex102.py
@@ -1,10 +1,15 @@
 # crie função fatorial() que receba 2 parâmetros: o primeiro que indique o número a calcular e outro chamado "show" que será um valor lógico opcional indicando se será mostrado ou não o processo de cálculo.
-def fatorial(num, show=0):
-    n = num
-    for c in range(num - 1, 1, -1):
+def fatorial(num, show=False):
+    n = 1
+    for c in range(num, 0, -1):
+        if show:
+            print(f'{c}', end='')
+            if c > 1:
+                print(' x ', end='')
+            else:
+                print(' = ', end='')
         n *= c
     return n
 
 
-
-fatorial(3)
+print(fatorial(5, False))
