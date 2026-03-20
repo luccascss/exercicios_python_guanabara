f75fdfb47b350dfedbd4f002d11b8c85e50d7151
Lucas Silva <lucascss11@gmail.com>
2026-03-20 17:18:42 -0300

    refactor: replace print in function
    - add return to def voto()

--- ex101.py
+++ ex101.py
@@ -1,16 +1,17 @@
 # crie um função chamada voto() que receba o parâmetro ano de nascimento e retorne um valor literal indicando: voto negado, voto opcional ou voto obrigatório.
-def voto(a):
+def voto(ano):
     from datetime import date
-    idade = date.today().year - a
+    idade = date.today().year - ano
     if 120 >= idade >= 65 or idade == 16 or idade == 17:
-        print(f'Com {idade} anos: VOTO OPCIONAL.')
+        return f'Com {idade} anos: VOTO OPCIONAL.'
     elif 16 > idade >= 0:
-        print(f'Com {idade} anos: VOTO NEGADO.')
+        return f'Com {idade} anos: VOTO NEGADO.'
     elif 65 > idade >= 18:
-        print(f'Com {idade} anos: VOTO OBRIGATÓRIO')
+        return f'Com {idade} anos: VOTO OBRIGATÓRIO'
     else:
-        print('Valores inválidos.')
+        return 'Valores inválidos.'
 
 ano_nasc = int(input('Em que ano você nasceu? '))
-voto(ano_nasc)
+print(voto(ano_nasc))
+
 
